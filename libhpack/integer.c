14f97b2d2bef9a6f54210beb72a2fef52078ceb8
Alvaro Lopez Ortega <alvaro@gnu.org>
2013-12-25 23:39:48 +0100

    Completes the initial implementation of the integer encoding function along with its unit tests.

--- libhpack/integer.c
+++ libhpack/integer.c
@@ -1,34 +1,77 @@
-// http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-05
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/* All files in libhpack are Copyright (C) 2014 Alvaro Lopez Ortega.
+ *
+ *   Authors:
+ *     * Alvaro Lopez Ortega <alvaro@gnu.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* Current ref:
+ * http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-05
+ */
 
 #include <stdio.h>
 #include <math.h>
 
 void
-integer_encode (int N, int value, char *mem, char *mem_len)
+integer_encode (int            N,        /* Prefix length in bits  */
+                int            value,    /* Number to encode       */
+                unsigned char *mem,      /* Memory to encode it to */
+                unsigned char *mem_len)  /* Memory used            */
 {
-	   int i = 0;
+    int i = 0;
 
-	   /* N is always between 1 and 8 bits [4.1.1.]
-	    */
-	   const int limits[] = {0, 1, 3, 7, 15, 31, 63, 127, 255};
-	   const int limit    = limits[N];
+    /* N is always between 1 and 8 bits [4.1.1.]
+     */
+    const unsigned char limits[] = {0, 1, 3, 7, 15, 31, 63, 127, 255};
+    const unsigned char limit    = limits[N];
 
-	   /*
-	    */
-	   if (value < limit) {
-			 mem[i++] = (char)value;
-			 *mem_len = i;
-			 return;
-	   } else {
-			 mem[i++] = (char)limit;
-			 value -= limit;
-			 while (value >= 128) {
-				    mem[i++] = value % 128 + 128;
-				    value /= 128;
-			 }
-			 mem[i++] = (char)value;
-			 *mem_len = i;
-	   }
+    /* An integer is represented in two parts:
+     * - A prefix that fills the current octet (N bits length)
+     * - An optional list of octets
+     */
+    if (value < limit) {
+        mem[i] = (mem[i] & ~limit) | (unsigned char)value;
+        *mem_len = ++i;
+        return;
+    }
 
-	   printf ("limit %d\n", limit);
+    /* the bits of the prefix are set to 1 */
+    mem[i] = (mem[i] & ~limit) | (unsigned char)limit;
+
+    /* allowed by an unsigned variable length integer */
+    i++;
+    value -= limit;
+    while (value >= 128) {
+        unsigned char t = (char)(128 + (value % 128));
+        mem[i++] = t;
+        value /= 128;
+    }
+
+    mem[i++] = (char)value;
+    *mem_len = i;
 }
--- test/integer.c
+++ test/integer.c
@@ -1,33 +1,142 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/* All files in libhpack are Copyright (C) 2014 Alvaro Lopez Ortega.
+ *
+ *   Authors:
+ *     * Alvaro Lopez Ortega <alvaro@gnu.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <check.h>
 #include "libhpack/integer.h"
 
-START_TEST (int1337_5bits)
+/* All examples came from:
+ * http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-05
+ */
+
+#define check_add_tc(suit,testcase,func)		\
+    do {                                        \
+        suite_add_tcase (suit, testcase);       \
+        tcase_add_test (testcase, func);        \
+    } while(0);
+
+
+START_TEST (int10_5bits)
 {
-	   char len = 0;
-	   char tmp[64];
+    unsigned char len   = 0xFF;
+    unsigned char tmp[] = {0,0};
 
-	   /* 4.1.1.2.  Example 2: Encoding 1337 using a 5-bit prefix
-	    * http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-05 */
-	   integer_encode (5, 1733, tmp, &len);
+    /* 4.1.1.1.  Example 1: Encoding 10 using a 5-bit prefix
+     */
+    integer_encode (5, 10, tmp, &len);
 
-	   /* Check output */
-	   ck_assert_int_eq (tmp[0], 31);
-	   ck_assert_int_eq (tmp[1], 154);
-	   ck_assert_int_eq (tmp[2], 10);
+    /* Check output */
+    ck_assert_int_eq (len, 1);
+    ck_assert_int_eq (tmp[0], 10);
 }
 END_TEST
+
+START_TEST (int1337_5bits)
+{
+    unsigned char len   = 0xFF;
+    unsigned char tmp[] = {0,0,0,0,0};
+
+    /* 4.1.1.2.  Example 2: Encoding 1337 using a 5-bit prefix
+     */
+    integer_encode (5, 1337, tmp, &len);
+
+    /* Check output */
+    ck_assert_int_eq (len, 3);
+    ck_assert_int_eq (tmp[0], 31);
+    ck_assert_int_eq (tmp[1], 154);
+    ck_assert_int_eq (tmp[2], 10);
+}
+END_TEST
+
+START_TEST (int42_8bits)
+{
+    unsigned char len   = 0xFF;
+    unsigned char tmp[] = {0,0};
+
+    /* 4.1.1.3.  Example 3: Encoding 42 starting at an octet-boundary
+     */
+    integer_encode (8, 42, tmp, &len);
+
+    /* Check output */
+    ck_assert_int_eq (len, 1);
+    ck_assert_int_eq (tmp[0], 42);
+}
+END_TEST
+
+START_TEST (int12_6bits)
+{
+    unsigned char len   = 0xFF;
+    unsigned char tmp[] = {0xC0,0};
+
+    integer_encode (6, 12, tmp, &len);
+
+    /* Check output */
+    ck_assert_int_eq (len, 1);
+    ck_assert_int_eq (tmp[0], 0xC0 | 12);
+}
+END_TEST
+
+START_TEST (int1338_5bits)
+{
+    unsigned char len   = 0xFF;
+    unsigned char tmp[] = {0xA0,0,0,0,0};
+
+    integer_encode (5, 1338, tmp, &len);
+
+    /* Check output */
+    ck_assert_int_eq (len, 3);
+    ck_assert_int_eq (tmp[0], 0xA0 | 31);
+    ck_assert_int_eq (tmp[1], 155);
+    ck_assert_int_eq (tmp[2], 10);
+}
+END_TEST
+
 
 int
 main (void)
 {
-	   Suite   *s1    = suite_create("IntEncoding");
-	   TCase   *tc1_1 = tcase_create("IntEncoding");
-	   SRunner *sr    = srunner_create(s1);
-	   int      nf;
+    Suite   *s1    = suite_create("Integer Encoding");
+    SRunner *sr    = srunner_create(s1);
+    TCase   *tc1_1 = tcase_create("5bits prefix");
+    TCase   *tc1_2 = tcase_create("5bits prefix w/ extra");
+    TCase   *tc1_3 = tcase_create("8bits prefix");
+    TCase   *tc1_4 = tcase_create("6bits prefix and data");
+    TCase   *tc1_5 = tcase_create("5bits prefix w/ extra and data");
 
-	   suite_add_tcase(s1, tc1_1);
-	   tcase_add_test(tc1_1, int1337_5bits);
+    check_add_tc (s1, tc1_1, int10_5bits);
+    check_add_tc (s1, tc1_2, int1337_5bits);
+    check_add_tc (s1, tc1_3, int42_8bits);
+    check_add_tc (s1, tc1_4, int12_6bits);
+    check_add_tc (s1, tc1_5, int1338_5bits);
 
-	   srunner_run_all(sr, CK_ENV);
-	   return srunner_ntests_failed(sr);
+    srunner_run_all(sr, CK_VERBOSE);
+    return srunner_ntests_failed(sr);
 }
