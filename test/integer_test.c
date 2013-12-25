587bb90f1c8bb750c2f6b1243890ea9b044f213e
Alvaro Lopez Ortega <alvaro@gnu.org>
2013-12-25 13:08:13 +0100

    Gets rids of the initial Google-test support (the C and C++ mixture for the test bench was a pain). A new bench based on Ctest + Check has been implemented.

---
+++ test/integer_test.c
@@ -0,0 +1,33 @@
+#include <check.h>
+#include "lib_hpack/integer.h"
+
+START_TEST (int1337_5bits)
+{
+	   char len = 0;
+	   char tmp[64];
+
+	   /* 4.1.1.2.  Example 2: Encoding 1337 using a 5-bit prefix
+	    * http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-05 */
+	   integer_encode (5, 1733, tmp, &len);
+
+	   /* Check output */
+	   ck_assert_int_eq (tmp[0], 31);
+	   ck_assert_int_eq (tmp[1], 154);
+	   ck_assert_int_eq (tmp[2], 10);
+}
+END_TEST
+
+int
+main (void)
+{
+	   Suite   *s1    = suite_create("IntEncoding");
+	   TCase   *tc1_1 = tcase_create("IntEncoding");
+	   SRunner *sr    = srunner_create(s1);
+	   int      nf;
+
+	   suite_add_tcase(s1, tc1_1);
+	   tcase_add_test(tc1_1, int1337_5bits);
+
+	   srunner_run_all(sr, CK_ENV);
+	   return srunner_ntests_failed(sr);
+}
