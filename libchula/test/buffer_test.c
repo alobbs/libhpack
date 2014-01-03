c23358ab9dd689cd3b0f082f509b85b20eea832a
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-01-03 16:00:44 +0100

    Adds a new QA bench entry to test chula_buffer_escape_arg()

--- libchula/test/buffer_test.c
+++ libchula/test/buffer_test.c
@@ -931,10 +931,28 @@ START_TEST (escape_uri_delims)
     ck_assert (a.len == 0);
     ck_assert (b.len == 0);
 
-    chula_buffer_add_str (&a, "hola \x00\x01\x1f :?#[]@ \x7f\xff adios!");
+    chula_buffer_add_str (&a, "hola \x00\x01\x1f :?#[]@ \x7f\xff adios!<>{}-_");
     ret = chula_buffer_escape_uri_delims(&b, &a);
     ck_assert (ret == ret_ok);
-    ck_assert_str_eq (b.buf, "hola%20%00%01%1f%20%3a%3f%23%5b%5d%40%20%7f%ff%20adios!");
+    ck_assert_str_eq (b.buf, "hola%20%00%01%1f%20%3a%3f%23%5b%5d%40%20%7f%ff%20adios!<>{}-_");
+}
+END_TEST
+
+START_TEST (escape_arg)
+{
+    ret_t           ret;
+    chula_buffer_t  a    = CHULA_BUF_INIT;
+    chula_buffer_t  b    = CHULA_BUF_INIT;
+
+    ret = chula_buffer_escape_arg (&b, &a);
+    ck_assert (ret == ret_ok);
+    ck_assert (a.len == 0);
+    ck_assert (b.len == 0);
+
+    chula_buffer_add_str (&a, "hola \x00\x01\x1f #%&+? \x7f\xff adios!@[]<>");
+    ret = chula_buffer_escape_arg(&b, &a);
+    ck_assert (ret == ret_ok);
+    ck_assert_str_eq (b.buf, "hola%20%00%01%1f%20%23%25%26%2b%3f%20%7f%ff%20adios!@[]<>");
 }
 END_TEST
 
@@ -975,6 +993,7 @@ buffer_tests (void)
     check_add (s1, unescape_uri);
     check_add (s1, escape_uri);
     check_add (s1, escape_uri_delims);
+    check_add (s1, escape_arg);
     run_test (s1);
 }
 
