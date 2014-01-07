b3650aa513eb33eaee620e7633acc00178376e09
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-01-07 08:04:00 +0100

    Adds a new QA test
    Adds a new test to try to cover some of the balancing functions that are currently not being executed in the test bench. Hopefully it'll be enough.

--- libchula/test/avl_test.c
+++ libchula/test/avl_test.c
@@ -46,6 +46,9 @@ START_TEST (new_)
     ret = chula_avl_new (&avl);
     ck_assert (avl != NULL);
     ck_assert (ret == ret_ok);
+
+    ret = chula_avl_free (AVL_GENERIC(avl), NULL);
+    ck_assert (ret == ret_ok);
 }
 END_TEST
 
@@ -139,9 +142,9 @@ END_TEST
 
 START_TEST (len)
 {
-    ret_t           ret;
-    size_t          len;
-    chula_avl_t     avl;
+    ret_t       ret;
+    size_t      len;
+    chula_avl_t avl;
 
     /* None */
     chula_avl_init (&avl);
@@ -169,15 +172,46 @@ START_TEST (len)
 }
 END_TEST
 
+START_TEST (stress)
+{
+    ret_t       ret;
+    size_t      len;
+    chula_avl_t avl;
+    int         pi[] = {3, 14, 15, 92, 65, 35, 89, 79, 32, 38, 46, 26, 43, 38, 32, 79, 50};
+
+    ret = chula_avl_init (&avl);
+    ck_assert (ret == ret_ok);
+
+    /* Add */
+    for (int i=0; i<16; i++) {
+        char tmp[10];
+        snprintf (tmp, 10, "%d", pi[i]);
+        ret = chula_avl_add_ptr (&avl, tmp, NULL);
+    }
+
+    /* Remove */
+    for (int i=15; i>=0; i--) {
+        char tmp[10];
+        snprintf (tmp, 10, "%d", pi[i]);
+        ret = chula_avl_del_ptr (&avl, tmp, NULL);
+    }
+
+    ret = chula_avl_mrproper (AVL_GENERIC(&avl), NULL);
+    ck_assert (ret == ret_ok);
+}
+END_TEST
+
 
 int
 avl_tests (void)
 {
     Suite *s1 = suite_create("AVL");
+
     check_add (s1, new_);
     check_add (s1, empty);
     check_add (s1, _ptr);
     check_add (s1, _buf);
     check_add (s1, len);
+    check_add (s1, stress);
     run_test (s1);
 }
