2ab286ee572bb5dbd1d036ac1cdf9a5962bf7a25
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-04-25 11:24:43 +0200

    Fixes typo

--- CI/lib-usage-1.c
+++ CI/lib-usage-1.c
@@ -9,7 +9,7 @@ main()
     hpack_header_table_t *table;
 
     ret = hpack_header_table_new (&table);
-    if (ret ! = ret_ok) return 1;
+    if (ret != ret_ok) return 1;
 
     hpack_header_table_free (table);
     return 0;
