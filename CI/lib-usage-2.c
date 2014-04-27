2b37ac720c394515cb0d191d5ad4a355f87cb1d3
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-04-27 23:11:00 +0200

    Adds a new CI 'lib-usage' test

---
+++ CI/lib-usage-2.c
@@ -0,0 +1,16 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#include <libchula/buffer.h>
+
+int
+main()
+{
+    ret_t          ret;
+    chula_buffer_t buf = CHULA_BUF_INIT;
+
+    ret = chula_buffer_add_str (&buf, "test");
+    if (ret != ret_ok) return 1;
+
+    chula_buffer_mrproper(&buf);
+    return 0;
+}
--- CI/lib-usage.sh
+++ CI/lib-usage.sh
@@ -10,9 +10,15 @@ sudo make -C build install
 # Compile tests
 HPACK_CFLAGS=`pkg-config --cflags libhpack`
 HPACK_LIBS=`pkg-config --libs libhpack`
+CHULA_CFLAGS=`pkg-config --cflags libchula`
+CHULA_LIBS=`pkg-config --libs libchula`
 
 cd CI
 $CC -c -o lib-usage-1.o lib-usage-1.c ${HPACK_CFLAGS}
 $CC -o lib-usage-1 lib-usage-1.o ${HPACK_LIBS}
 ./lib-usage-1
+
+$CC -c -o lib-usage-2.o lib-usage-2.c ${CHULA_CFLAGS}
+$CC -o lib-usage-2 lib-usage-2.o ${CHULA_LIBS}
+./lib-usage-2
 cd ..
