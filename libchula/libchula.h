0f8224b6cae91586acfd4592687a04a5b8e350cb
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-04-28 11:33:46 +0200

    Adds libchula's primary header

---
+++ libchula/libchula.h
@@ -0,0 +1,56 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/* All files in libchula are Copyright (C) 2014 Alvaro Lopez Ortega.
+ *
+ *   Authors:
+ *     * Alvaro Lopez Ortega <alvaro@alobbs.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CHULA_H
+#define CHULA_H
+
+#define CHULA_H_INSIDE
+
+#include <libchula/avl.h>
+#include <libchula/avl_generic.h>
+#include <libchula/buffer.h>
+#include <libchula/common.h>
+#include <libchula/crc32.h>
+#include <libchula/debug.h>
+#include <libchula/list.h>
+#include <libchula/log.h>
+#include <libchula/logger_fd.h>
+#include <libchula/macros.h>
+#include <libchula/md5.h>
+#include <libchula/missing_sysfuncs.h>
+#include <libchula/sha1.h>
+#include <libchula/sha512.h>
+#include <libchula/util.h>
+
+#undef CHULA_H_INSIDE
+
+#endif /* CHULA_H */
