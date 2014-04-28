e64c0a1f1fb81d7737beb5fbbbd9d6f505d892ef
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-04-28 11:46:24 +0200

    Adds libhpack's primary header

---
+++ libhpack/libhpack.h
@@ -0,0 +1,53 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/* All files in libchula are Copyright (C) 2014 Alvaro Lopez Ortega.
+ *
+ *   Authors:
+ *     * Alvaro Lopez Ortega <alvaro@alobbs.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef HPACK_H
+#define HPACK_H
+
+#define HPACK_H_INSIDE
+
+#include <libchula/libchula.h>
+
+#include <libhpack/bitmap_set.h>
+#include <libhpack/header_field.h>
+#include <libhpack/header_parser.h>
+#include <libhpack/header_store.h>
+#include <libhpack/header_table.h>
+#include <libhpack/huffman.h>
+#include <libhpack/huffman_tables.h>
+#include <libhpack/integer.h>
+#include <libhpack/libhpack.h>
+#include <libhpack/macros.h>
+
+#undef HPACK_H_INSIDE
+
+#endif /* HPACK_H */
