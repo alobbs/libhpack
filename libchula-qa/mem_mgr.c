f0eaf61cc0cb4c217c69366eb7788429ae1e22be
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-05-08 23:07:57 +0200

    Memory Manager Linux implementation

--- libchula-qa/mem_mgr.c
+++ libchula-qa/mem_mgr.c
@@ -37,6 +37,8 @@
 
 #ifdef HAVE_MALLOC_MALLOC_H
 # include <malloc/malloc.h>
+#elif defined (HAVE_MALLOC_H)
+# include <malloc.h>
 #endif
 
 #ifdef HAVE_SYS_MMAN_H
@@ -53,16 +55,22 @@ static chula_mem_mgr_t    *current_manager = NULL;
 ret_t
 chula_mem_mgr_init (chula_mem_mgr_t *mgr)
 {
-#ifdef HAVE_MALLOC_DEFAULT_ZONE
-    malloc_zone_t *zone = malloc_default_zone();
-
     /* Properties */
     mgr->frozen = false;
+
+#ifdef HAVE_MALLOC_DEFAULT_ZONE
+    malloc_zone_t *zone = malloc_default_zone();
 
     /* Store original functions */
     mgr->system.malloc  = zone->malloc;
     mgr->system.realloc = zone->realloc;
     mgr->system.free    = zone->free;
+
+#elif defined(HAVE_MALLOC_HOOK)
+    /* Store original hook functions */
+    mgr->system.malloc  = __malloc_hook;
+    mgr->system.realloc = __realloc_hook;
+    mgr->system.free    = __free_hook;
 #endif
 
    return ret_ok;
@@ -75,15 +83,67 @@ chula_mem_mgr_mrproper (chula_mem_mgr_t *mgr)
     return ret_ok;
 }
 
+#ifdef HAVE_MALLOC_DEFAULT_ZONE
+# define FUNC_MALLOC(name)  static void *_ ## name ## _malloc  (struct _malloc_zone_t *zone, size_t size)
+# define FUNC_REALLOC(name) static void *_ ## name ## _realloc (struct _malloc_zone_t *zone, void *ptr, size_t size)
+# define FUNC_FREE(name)    static void  _ ## name ## _free    (struct _malloc_zone_t *zone, void *ptr)
+# define CALL_MALLOC                                                    \
+    ({                                                                  \
+        void* (*orig)(struct _malloc_zone_t *, size_t ) =               \
+            current_manager->system.malloc;                             \
+        orig (zone, size);                                              \
+    })
+# define CALL_REALLOC                                                   \
+    ({                                                                  \
+        void * (*orig)(struct _malloc_zone_t *, void *, size_t) =       \
+            current_manager->system.realloc;                            \
+        orig (zone, ptr, size);                                         \
+    })
+# define CALL_FREE                                                      \
+    ({                                                                  \
+        void (*orig)(struct _malloc_zone_t *, void *) =                 \
+            current_manager->system.free;                               \
+        orig (zone, ptr);                                               \
+    })
+
+#elif defined(HAVE_MALLOC_HOOK)
+# define FUNC_MALLOC(name)  static void *_ ## name ## _malloc  (size_t size, const void *caller)
+# define FUNC_REALLOC(name) static void *_ ## name ## _realloc (void *ptr, size_t size, const void *caller)
+# define FUNC_FREE(name)    static void  _ ## name ## _free    (void *ptr, const void *caller)
+# define CALL_MALLOC                                                    \
+    ({                                                                  \
+        void* (*orig)(size_t, const void *) =                           \
+            current_manager->system.malloc;                             \
+        orig (size, caller);                                            \
+    })
+# define CALL_REALLOC                                                   \
+    ({                                                                  \
+        void * (*orig)(void *, size_t, const void *) =                  \
+            current_manager->system.realloc;                            \
+        orig (ptr, size, caller);                                       \
+    })
+# define CALL_FREE                                                      \
+    ({                                                                  \
+        void (*orig)(void *, const void *) =                            \
+            current_manager->system.free;                               \
+        orig (ptr, caller);                                             \
+    })
+#endif
+
 ret_t
 chula_mem_mgr_set_policy (chula_mem_mgr_t    *mgr,
                           chula_mem_policy_t *policy)
 {
+    /* Assign globals */
+    current_manager = mgr;
+    current_policy  = policy;
+
+    /* Set new functions (Darwin)
+     */
 #ifdef HAVE_MALLOC_DEFAULT_ZONE
     int            re;
     malloc_zone_t *zone = malloc_default_zone();
 
-    /* Set new functions */
     re = mprotect (zone, getpagesize(), PROT_READ|PROT_WRITE);
     if (re != 0) return ret_error;
 
@@ -91,8 +151,12 @@ chula_mem_mgr_set_policy (chula_mem_mgr_t    *mgr,
     zone->realloc = policy->realloc;
     zone->free    = policy->free;
 
-    current_manager = mgr;
-    current_policy  = policy;
+    /* Set new functions (Linux)
+     */
+#elif defined(HAVE_MALLOC_HOOK)
+    __malloc_hook  = policy->malloc;
+    __realloc_hook = policy->realloc;
+    __free_hook    = policy->free;
 #endif
 
     return ret_ok;
@@ -142,10 +206,9 @@ chula_mem_policy_mrproper (chula_mem_policy_t *policy)
 /* Random Failures Memory Policy
  */
 
-static void *
-_random_malloc (struct _malloc_zone_t *zone, size_t size)
+
+FUNC_MALLOC (random)
 {
-    void* (*orig)(struct _malloc_zone_t *zone, size_t size) = current_manager->system.malloc;
     chula_mem_policy_random_t *policy = (chula_mem_policy_random_t *)current_policy;
 
     if (! current_manager->frozen) {
@@ -154,13 +217,11 @@ _random_malloc (struct _malloc_zone_t *zone, size_t size)
         }
     }
 
-    return orig (zone, size);
+    return CALL_MALLOC;
 }
 
-static void *
-_random_realloc (struct _malloc_zone_t *zone, void *ptr, size_t size)
+FUNC_REALLOC (random)
 {
-    void * (*orig)(struct _malloc_zone_t *zone, void *ptr, size_t size) = current_manager->system.realloc;
     chula_mem_policy_random_t *policy = (chula_mem_policy_random_t *)current_policy;
 
     if (! current_manager->frozen) {
@@ -169,14 +230,12 @@ _random_realloc (struct _malloc_zone_t *zone, void *ptr, size_t size)
         }
     }
 
-    return orig (zone, ptr, size);
+    return CALL_REALLOC;
 }
 
-static void
-_random_free (struct _malloc_zone_t *zone, void *ptr)
+FUNC_FREE (random)
 {
-    void * (*orig)(struct _malloc_zone_t *zone, void *ptr) = current_manager->system.free;
-    orig (zone, ptr);
+    CALL_FREE;
 }
 
 ret_t
@@ -203,43 +262,37 @@ chula_mem_policy_random_mrproper (chula_mem_policy_random_t *policy)
 /* Counter Memory Policy
  */
 
-static void *
-_counter_malloc (struct _malloc_zone_t *zone, size_t size)
+FUNC_MALLOC (counter)
 {
-    void* (*orig)(struct _malloc_zone_t *zone, size_t size) = current_manager->system.malloc;
     chula_mem_policy_counter_t *policy = (chula_mem_policy_counter_t *)current_policy;
 
     if (! current_manager->frozen) {
         policy->n_malloc++;
     }
 
-    return orig (zone, size);
+    return CALL_MALLOC;
 }
 
-static void *
-_counter_realloc (struct _malloc_zone_t *zone, void *ptr, size_t size)
+FUNC_REALLOC (counter)
 {
-    void * (*orig)(struct _malloc_zone_t *zone, void *ptr, size_t size) = current_manager->system.realloc;
     chula_mem_policy_counter_t *policy = (chula_mem_policy_counter_t *)current_policy;
 
     if (! current_manager->frozen) {
         policy->n_realloc++;
     }
 
-    return orig (zone, ptr, size);
+    return CALL_REALLOC;
 }
 
-static void
-_counter_free (struct _malloc_zone_t *zone, void *ptr)
+FUNC_FREE (counter)
 {
-    void * (*orig)(struct _malloc_zone_t *zone, void *ptr) = current_manager->system.free;
     chula_mem_policy_counter_t *policy = (chula_mem_policy_counter_t *)current_policy;
 
     if (! current_manager->frozen) {
         policy->n_free++;
     }
 
-    orig (zone, ptr);
+    CALL_FREE;
 }
 
 ret_t
@@ -264,10 +317,8 @@ chula_mem_policy_counter_mrproper (chula_mem_policy_counter_t *polcnt)
 /* Scheduled Failure Memory Policy
  */
 
-static void *
-_after_malloc (struct _malloc_zone_t *zone, size_t size)
+FUNC_MALLOC (after)
 {
-    void* (*orig)(struct _malloc_zone_t *zone, size_t size) = current_manager->system.malloc;
     chula_mem_policy_sched_fail_t *policy = (chula_mem_policy_sched_fail_t *)current_policy;
 
     policy->counter++;
@@ -278,13 +329,11 @@ _after_malloc (struct _malloc_zone_t *zone, size_t size)
         return NULL;
     }
 
-    return orig (zone, size);
+    return CALL_MALLOC;
 }
 
-static void *
-_after_realloc (struct _malloc_zone_t *zone, void *ptr, size_t size)
+FUNC_REALLOC (after)
 {
-    void * (*orig)(struct _malloc_zone_t *zone, void *ptr, size_t size) = current_manager->system.realloc;
     chula_mem_policy_sched_fail_t *policy = (chula_mem_policy_sched_fail_t *)current_policy;
 
     policy->counter++;
@@ -295,20 +344,18 @@ _after_realloc (struct _malloc_zone_t *zone, void *ptr, size_t size)
         return NULL;
     }
 
-    return orig (zone, ptr, size);
+    return CALL_REALLOC;
 }
 
-static void
-_after_free (struct _malloc_zone_t *zone, void *ptr)
+FUNC_FREE (after)
 {
-    void * (*orig)(struct _malloc_zone_t *zone, void *ptr) = current_manager->system.free;
     chula_mem_policy_sched_fail_t *policy = (chula_mem_policy_sched_fail_t *)current_policy;
 
     if (! current_manager->frozen) {
         policy->counter++;
     }
 
-    orig (zone, ptr);
+    CALL_FREE;
 }
 
 
