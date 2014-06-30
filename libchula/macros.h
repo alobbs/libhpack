f53f2ba3d603163770e3c201cf520731f38aa3c4
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-06-30 23:10:47 +0200

    Fixes likely() and unlikely() macros

--- libchula/macros.h
+++ libchula/macros.h
@@ -63,16 +63,12 @@
 #endif
 
 /* Compiler options */
-#if defined(__GNUC__) && (__GNUC__ > 2) && defined(__OPTIMIZE__)
+#if defined(__GNUC__) && (__GNUC__ > 2)
 # define likely(x)   __builtin_expect((x),1)
 # define unlikely(x) __builtin_expect((x),0)
-# define not_ok(x)   __builtin_expect(x != ret_ok, 0)
-# define lt_ok(x)    __builtin_expect(x <  ret_ok, 0)
 #else
 # define likely(x)   (x)
 # define unlikely(x) (x)
-# define not_ok(x)   (x != ret_ok)
-# define lt_ok(x)    (x <  ret_ok)
 #endif
 
 #if __GNUC_VERSION >= 30000
