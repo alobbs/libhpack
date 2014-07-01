fee0e7eb91c58ecb67d008d4b5fd419036426294
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-07-01 23:08:22 +0200

    First few bits for hpack_header_encoder_render()

--- libhpack/header_encoder.c
+++ libhpack/header_encoder.c
@@ -84,12 +84,78 @@ hpack_header_encoder_add_field (hpack_header_encoder_t *enc,
     return hpack_header_store_emit (&enc->store, field);
 }
 
+static ret_t
+add_string (chula_buffer_t *output,
+            chula_buffer_t *in,
+            bool            huffman)
+{
+    ret_t   ret;
+    uint8_t mem_len = 16;
+    uint8_t mem[16] = {[0 ... 15] =  0};
+
+    /* TODO */
+    UNUSED(huffman);
+
+    /* Length */
+    ret = hpack_integer_encode (7, in->len, mem, &mem_len);
+    if (unlikely (ret != ret_ok)) return ret;
+
+    /* Content */
+    chula_buffer_add_RET (output, (const char *)mem, mem_len);
+    chula_buffer_add_buffer_RET (output, in);
+
+    return ret_ok;
+}
+
+static ret_t
+field_encode_plain (hpack_header_encoder_t *enc,
+                    hpack_header_field_t   *field,
+                    chula_buffer_t         *output)
+{
+    ret_t   ret;
+
+    UNUSED (enc);
+
+    /* Literal Header Field without Indexing - New Name
+     *
+     *     0   1   2   3   4   5   6   7
+     *   +---+---+---+---+---+---+---+---+
+     *   | 0 | 0 | 0 | 0 |       0       |
+     *   +---+---+-----------------------+
+     *   | H |     Name Length (7+)      |
+     *   +---+---------------------------+
+     *   |  Name String (Length octets)  |
+     *   +---+---------------------------+
+     *   | H |     Value Length (7+)     |
+     *   +---+---------------------------+
+     *   | Value String (Length octets)  |
+     *   +-------------------------------+
+     */
+    chula_buffer_add_char_RET (output, '\0');
+
+    /* Name */
+    ret = add_string (output, &field->name, false);
+    if (unlikely (ret != ret_ok)) return ret;
+
+    ret = add_string (output, &field->value, false);
+    if (unlikely (ret != ret_ok)) return ret;
+
+    return ret_ok;
+}
 
 ret_t
 hpack_header_encoder_render (hpack_header_encoder_t *enc,
                              chula_buffer_t         *output)
 {
-    UNUSED(enc);
-    UNUSED(output);
+    ret_t                       ret;
+    hpack_header_store_entry_t *i;
+
+    hpack_header_store_foreach (i, &enc->store) {
+        hpack_header_field_t *field = HPACK_HEADER_FIELD(i);
+
+        ret = field_encode_plain (enc, field, output);
+        if (unlikely (ret != ret_ok)) return ret;
+    }
+
     return ret_ok;
 }
